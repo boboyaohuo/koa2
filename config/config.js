ba9aea48d3135cc1eb10edc41159a35271d52aae
吴建波 <wujianbo0429@163.com>
2020-05-20 18:12:02 +0800

    'mysql'

---
+++ config/config.js
@@ -0,0 +1,15 @@
+//config.js
+const config = {
+	// 启动端口
+	port: 3000,
+	// 数据库配置
+	database: {
+		DATABASE: 'boboyaohuo', //数据库名称
+		USERNAME: 'root', //mysql用户名
+		PASSWORD: 'WUji1123581321', //mysql密码
+		PORT: '3306', //mysql端口号
+		HOST: '192.168.148.131' //服务器ip
+	}
+};
+
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,13 @@
-const router = require('koa-router')()
+const router = require('koa-router')();
+const service = require('../lib/mysqlConfig');
 
 router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!',
-    intro: 'boboyaohuo'
-  })
-})
+	let res = await service.getIndexLove();
+	ctx.body = {
+		data: res,
+		code: 0,
+		message: ''
+	};
+});
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
-
-router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
-  }
-})
-
-module.exports = router
+module.exports = router;
