3a4a85599f070390bcb205461f79297d6b417c48
吴建波 <wujianbo0429@163.com>
2020-06-12 16:40:12 +0800

    charset设置，自动同步创建表

--- models/index.js
+++ models/index.js
@@ -2,13 +2,14 @@ const sequelize = require('./sequelize.js');
 const Sequelize = require('sequelize');
 
 const Index = sequelize.define('t_index', {
-	text: {
-		type: Sequelize.STRING(500),
-		allowNull: false,
-		comment: '文本'
-	}
+  text: {
+    type: Sequelize.STRING(500),
+    allowNull: false,
+    comment: '文本'
+  }
 });
 
-Index.sync(); // 创建表
+// Index.sync({ alter: true }); // 自动同步创建表 （不安全）
+Index.sync(); // 没有表就创建表，有表无操作
 
 module.exports = Index;
--- models/sequelize.js
+++ models/sequelize.js
@@ -3,14 +3,15 @@ const config = require('../config/config');
 
 // 链接数据库
 const sequelize = new Sequelize(config.database.DATABASENAME, config.database.USERNAME, config.database.PASSWORD, {
-	host: config.database.HOST,
-	port: config.database.PORT,
-	dialect: config.database.DIALECT,
-	underscored: true, // 字段以下划线（_）来分割（默认是驼峰命名风格）
-	timezone: config.database.timezone, // 东八区
-	define: {
-		freezeTableName: true
-	}
+  host: config.database.HOST,
+  port: config.database.PORT,
+  dialect: config.database.DIALECT,
+  underscored: true, // 字段以下划线（_）来分割（默认是驼峰命名风格）
+  timezone: config.database.timezone, // 东八区
+  define: {
+    freezeTableName: true,
+    charset: 'utf8mb4'
+  }
 });
 
 module.exports = sequelize;
